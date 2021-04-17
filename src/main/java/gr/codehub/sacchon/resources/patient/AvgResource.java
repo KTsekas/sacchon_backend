be6903f77d869f38551f43e762b2c7c7b6045991
Christos Damianidis <chris.damianidis2@gmail.com>
2021-04-17 20:42:09 +0300

    Fixed AvgResource for Date changes

--- src/main/java/gr/codehub/sacchon/resources/patient/AvgResource.java
+++ src/main/java/gr/codehub/sacchon/resources/patient/AvgResource.java
@@ -10,9 +10,36 @@ import org.restlet.data.Status;
 import org.restlet.resource.Get;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
 
 public class AvgResource extends AuthResource {
 
+    @Get("json")
+    public SingleValueRepresentation<Double> doAvg() {
+        String type = getAttribute("type");
+        if (type == null) {
+            setStatus(Status.SERVER_ERROR_INTERNAL, "False routing from server");
+            return null;
+        }
+        Optional<LocalDate> start = getDateQueryValue("start",LocalDate.of(-9999,1,1));
+        if (start.isEmpty()) {
+            setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid start date");
+            return null;
+        }
+        Optional<LocalDate> end = getDateQueryValue("end",LocalDate.of(9999,1,1));
+        if (end.isEmpty()) {
+            setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid end date");
+            return null;
+        }
+        if (type.equals("carb"))
+            return getCarbAverage(start.get(), end.get());
+        else if (type.equals("glucose"))
+            return getGlucoseAverage(start.get(), end.get());
+        else
+            return new SingleValueRepresentation<>(0.0);
+    }
     private SingleValueRepresentation<Double> getCarbAverage(LocalDate start, LocalDate end) {
         CarbService srv = new CarbService((Patient) getUser());
         setService(srv);
@@ -25,34 +52,16 @@ public class AvgResource extends AuthResource {
         return new SingleValueRepresentation<>(srv.getAverage(start, end));
     }
 
-    @Get("json")
-    public SingleValueRepresentation<Double> doAvg() {
-        String type = getAttribute("type");
-        if (type == null) {
-            setStatus(Status.SERVER_ERROR_INTERNAL, "False routing from server");
-            return null;
-        }
-        String start = this.getQueryValue("start");
-        String end = this.getQueryValue("end");
-//        LocalDate start_date = DateHelper.getIfDateOrDefault(start, DateHelper.MIN);
-//        LocalDate end_date = DateHelper.getIfDateOrDefault(end, DateHelper.MAX);
-        LocalDate start_date=null;
-        LocalDate end_date=null;
-        if (start_date == null) {
-            setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid start date");
-            return null;
-        }
-        if (end_date == null){
-            setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid end date");
-            return null;
-        }
-        if (type.equals("carb"))
-            return getCarbAverage(start_date, end_date);
-        else if (type.equals("glucose"))
-            return getGlucoseAverage(start_date, end_date);
-        else
-            return new SingleValueRepresentation<>(0.0);
-    }
 
+    private Optional<LocalDate> getDateQueryValue(String name,LocalDate def){
+        String q = this.getQueryValue(name);
+        if ( q == null )
+            return Optional.of(def);
+        try{
+            return Optional.of(LocalDate.parse(q, DateTimeFormatter.ISO_LOCAL_DATE));
+        }catch(DateTimeParseException ex){
+            return Optional.empty();
+        }
+    }
 
 }
